8dc8b766b0b396ff97d1fa364741f1f55e0bb4d0
Kriti Atrishi <kritiatrishi@gmail.com>
2024-07-13 21:47:10 +0530

    RabinKarp

--- src/main/java/org/practice/collection/HashMaps.java
+++ src/main/java/org/practice/collection/HashMaps.java
@@ -30,11 +30,41 @@ public class HashMaps {
         }
         public void remove(String key){
             int hash=Math.abs(key.hashCode()%entities.length);
-            if(entities[hash]!=null&&entities[hash].key.equals(key)){
-               entities[hash]=null;
+            if(entities[hash]!=null&&entities[hash].key.equals(key)) {
+                entities[hash] = null;
             }
-
         }
+    //-------------------------------------------------------------
+    //RabinKarp String Matching
+    //-------------------------------------------------------------
+    private final int PRIME=101;
+    public long calculateHash(String s){
+        long hash=0;
+        for (int i = 0; i < s.length(); i++) {
+            hash= (long) (hash+s.charAt(i)*Math.pow(PRIME,i));
+        }
+        return hash;
+    }
+    public long updateHash(long prevHash,char oldC,char newC,int patternLength){
+        long newHash=(prevHash-oldC)/PRIME;
+        newHash=newHash+newC*(long)Math.pow(PRIME,patternLength-1);
+        return newHash;
+    }
+    public void search(String text,String pattern){
+        int patternLength=pattern.length();
+        long patternHash=calculateHash(pattern);
+        long textHash=calculateHash(text.substring(0,patternLength));
+        for (int i = 0; i < text.length()-patternLength; i++) {
+            if(textHash==patternHash){
+                if(text.substring(i,i+patternLength).equals(pattern)){
+                    System.out.println("Pattern found at index: "+i);
+                }
+            }
+            if(i<text.length()-patternLength){
+                textHash=updateHash(textHash,text.charAt(i),text.charAt(i+patternLength),patternLength);
+            }
+        }
+    }
     public static void main(String[] args) {
         HashMap<String,Integer> map=new HashMap<>();
         HashMaps maps=new HashMaps();
@@ -55,5 +85,9 @@ public class HashMaps {
         set.add(42);
         set.add(9);
         System.out.println(set);
+        System.out.println("--------------------------------");
+        System.out.println("--------------------------------");
+        maps.search("fwnshuirgsfubj","fub");
+        System.out.println("--------------------------------");
     }
 }
